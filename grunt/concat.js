23cc0eabfa9ad693fa42146059ca5be1112fc5a1
dutscher <dutscher_sbf@hotmail.com>
2016-02-18 18:03:22 +0100

    prepared new world changing

--- grunt/concat.js
+++ grunt/concat.js
@@ -8,7 +8,7 @@ module.exports = {
                 "/*",
                 "The MIT License (MIT)",
                 "",
-                "Copyright (c) 2015 willy woitas http://trialstrackmap.sb-f.de",
+                "Copyright (c) 2016 willy woitas http://trialstrackmap.sb-f.de",
                 "",
                 "Permission is hereby granted, free of charge, to any person obtaining a copy",
                 "of this software and associated documentation files (the 'Software'), to deal",
