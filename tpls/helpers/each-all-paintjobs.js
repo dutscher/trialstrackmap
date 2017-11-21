05daf802b9d5b073fdd8318bf161049b533da11c
dutscher <dutscher_sbf@hotmail.com>
2017-11-21 22:26:03 +0100

    solved newest paintjobs

--- tpls/helpers/each-all-paintjobs.js
+++ tpls/helpers/each-all-paintjobs.js
@@ -14,23 +14,24 @@
             // increase for footer
             maxPaintjobs++;
 
-            for (var paintJobId = 0; paintJobId < maxPaintjobs; paintJobId++) {
+            for (var paintJobId = -1; paintJobId < maxPaintjobs; paintJobId++) {
                 html += "<tr>";
                 for (var i in bikesPaintjobs.bikeSorting) {
                     var bikeId = bikesPaintjobs.bikeSorting[i],
                         paintJobExist = paintJobId in bikesPaintjobs.bikes[bikeId],
-                        paintjobLength = bikesPaintjobs.bikes[bikeId].length,
-                        showFooter = paintjobLength == paintJobId;
+                        paintjobLength = bikesPaintjobs.bikes[bikeId].length;
 
-                    if (!showFooter) {
-                        html += '<td class="sprite-col-item' + (!paintJobExist ? " is-empty" : "") + '" title="' + (paintJobExist ? bikesPaintjobs.bikes[bikeId][paintJobId].name : "") + '">';
+                    // header
+                    if (paintJobId === -1) {
+                        html += '<td class="sprite-col-subheader"><div class="sprite-col-subheader-label">' + paintjobLength + '</div></td>'
+                    } else {
+                        html += '<td class="sprite-col-item' + (!paintJobExist ? " is-empty" : "") + '" title="'
+                            + (paintJobExist ? bikesPaintjobs.bikes[bikeId][paintJobId].name : "") + '">';
                         if (paintJobExist) {
                             html += '<i class="paintjob-icon paintjob-icon-' + bikeId + '-' + paintJobId + '"></i>';
                             html += '<i class="paintjob paintjob-' + bikeId + '-' + paintJobId + '"></i>';
                         }
                         html += "</td>\n";
-                    } else {
-                        html += '<td class="sprite-col-footer"><div class="sprite-col-footer-label">' + paintjobLength + '</div></td>'
                     }
                 }
                 html += "</tr>\n";
